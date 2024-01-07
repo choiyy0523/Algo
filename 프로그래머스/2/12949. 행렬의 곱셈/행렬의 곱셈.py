3a1e1d3476aa973bd6ec5dc88b9e56c31c5dca2c
Choi Yunyoung <97656369+choiyy0523@users.noreply.github.com>
2024-01-07 22:37:20 +0900

    [level 2] Title: 행렬의 곱셈, Time: 16.14 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,14 @@
+def solution(arr1, arr2):
+    M = len(arr1[0])   # arr1 가로  2
+    N = len(arr1)      # arr1 세로  3
+    J = len(arr2[0])   # arr2 가로  2
+    K = len(arr2)      # arr2 세로  2
+    
+    ans = [[0] * J for _ in range(N)]  # 2, 3 [[0, 0], [0, 0], [0, 0]]
+    
+    for n in range(N):   
+        for j in range(J):  
+            for m in range(M):
+                ans[n][j] += arr1[n][m] * arr2[m][j] 
+
+    return ans
